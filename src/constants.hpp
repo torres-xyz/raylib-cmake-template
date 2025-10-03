c6469d4d81b301c9451374c9f44857607e52af96
torres-xyz <43039035+torres-xyz@users.noreply.github.com>
2025-10-03 17:05:03 +0100

    Debug and Release builds

--- src/constants.hpp
+++ src/constants.hpp
@@ -4,6 +4,17 @@ namespace constants
 {
     // Inline variables have external linkage by default, so that they are visible to the linker.
     // This is necessary so the linker can de-duplicate the definitions.
-    inline constexpr int screenWidth = 800;
-    inline constexpr int screenHeight = 450;
+
+    inline constexpr int imguiSideBarWidth{300};
+    inline constexpr int screenWidthOriginal{1000};
+    inline constexpr int screenWidth{screenWidthOriginal};
+    inline constexpr int screenHeight{1000};
+    inline constexpr int windowScreenWidth{
+#if(DEBUG)
+        screenWidthOriginal + imguiSideBarWidth
+#else
+        1000
+#endif
+    };
+    inline constexpr int windowScreenHeight{1000};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <random> // for std::random_device
 #include "constants.hpp"
-#include "raylib.h"
+// #include "raylib.h"
 #include "raylib-cpp.hpp"
 #include "imgui.h"
 #include "rlImGui.h"
@@ -13,14 +13,17 @@ int main()
     SetRandomSeed(rd());
 
     SetConfigFlags(FLAG_VSYNC_HINT);
-    raylib::Window window(constants::screenWidth, constants::screenHeight,
+    raylib::Window window(constants::windowScreenWidth, constants::windowScreenHeight,
                           "Raylib-cpp Template");
-    // InitWindow(constants::screenWidth, constants::screenHeight, "Raylib Template");
 
     SetTargetFPS(GetMonitorRefreshRate(0));
+
+#if (DEBUG)
     rlImGuiSetup(true);
+#endif
 
     const raylib::Texture2D mewTex("resources/mew.png");
+
 
     // Main game loop
     while (!window.ShouldClose()) // Detect window close button or ESC key
@@ -29,19 +32,22 @@ int main()
 
 
         // Draw ----------------------------------------------------------------
-        BeginDrawing();
         window.ClearBackground(RAYWHITE);
+
+#if (DEBUG)
         // start ImGui Content
         rlImGuiBegin();
 
         // show ImGui Content
-        bool open = true;
-        ImGui::ShowDemoWindow(&open);
-
-        if (ImGui::Begin("Test Window", &open))
+        bool open = false;
+        ImGui::SetNextWindowPos(ImVec2(constants::windowScreenWidth - constants::imguiSideBarWidth, 0));
+        ImGui::SetNextWindowSize(ImVec2(constants::imguiSideBarWidth, constants::windowScreenHeight));
+        if (ImGui::Begin("Test Window", &open,
+                         ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar))
         {
-            ImGui::TextUnformatted(ICON_FA_JEDI);
-
+            ImGui::TextUnformatted("My Text inside the window");
+            ImGui::TextUnformatted("My Text inside the window");
+            ImGui::TextUnformatted("My Text inside the window");
             rlImGuiImage(&mewTex);
         }
         ImGui::End();
@@ -49,11 +55,19 @@ int main()
         // end ImGui Content
         rlImGuiEnd();
 
+        raylib::Rectangle newRect{
+            constants::screenWidth * 0.5,
+            constants::screenHeight * 0.5,
+            constants::screenWidth * 0.5,
+            constants::screenHeight * 0.5
+        };
+        newRect.Draw(BLACK);
+#endif
+
         mewTex.Draw(constants::screenWidth / 2 - mewTex.width / 2,
                     constants::screenHeight / 2 - mewTex.height / 2,
                     WHITE);
 
-        DrawPixel(100, 10, BLACK);
 
         EndDrawing();
     }
